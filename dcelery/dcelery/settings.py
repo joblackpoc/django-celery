bcabb3f7a6ebb5519a4efa981eb3607102c10dfe
joblackpoc <duopoc@gmail.com>
2023-12-12 15:07:00 +0700

    finish first docker

--- dcelery/dcelery/settings.py
+++ dcelery/dcelery/settings.py
@@ -4,9 +4,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = os.environ.get("DEBUG")
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
+SECRET_KEY = os.environ.get('SECRETE_KEY')
+DEBUG = os.environ.get('DEBUG')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 # Application definition
 
